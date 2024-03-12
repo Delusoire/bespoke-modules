4569d00c847043808299c9640ff61919a3489e5e
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-12 14:46:05 +0000

    y

--- std/mixin.js
+++
@@ -1,6 +0,0 @@
-export let registerTransform;
-export default async function(rt) {
-    registerTransform = rt;
-    await import("./src/expose/index.js");
-    await import("./src/registers/registers.js");
-}
--- std/src/expose/PlatformAutoGen.d.js
+++
@@ -1,6 +0,0 @@
-/*
-const HU = await import("/hooks/util.js")
-const MSI = await import("/modules/Delusoire/std/index.js")
-HU.visited.clear()
-MSI.S.Platform.getClipboardAPI().copy(HU.type(MSI.S.Platform, "PlatformAutoGen"))
-*/ 
