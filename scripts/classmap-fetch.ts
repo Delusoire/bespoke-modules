38fbc8d79483433b63bea108c4092b6af2dd0da3
Delusoire <deluso7re@outlook.com>
2024-07-01 19:50:33 +0100

    fix: erroneous classmap declaration

--- scripts/classmap-fetch.ts
+++ scripts/classmap-fetch.ts
@@ -17,7 +17,7 @@ for await (const module of Deno.readDir("modules")) {
 		continue;
 	}
 
-	const classmapDts = genClassMapDts(classmap);
+	const classmapDts = genClassMapDts(JSON.parse(classmap));
 	const classmapDtsPath = path.join("modules", module.name, "classmap.d.ts");
 
 	await Deno.writeTextFile(classmapDtsPath, classmapDts);
