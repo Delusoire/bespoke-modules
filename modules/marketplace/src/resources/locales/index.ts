94d9cc2ec879bc14b15fc07a2e37a2b3cdae3ed6
Delusoire <deluso7re@outlook.com>
2024-08-23 12:56:44 +0100

    y

--- modules/marketplace/src/resources/locales/index.ts
+++ modules/marketplace/src/resources/locales/index.ts
@@ -1,7 +1,9 @@
 import type { ResourceLanguage } from "https://esm.sh/v135/i18next";
 
 const j = <R>(path: string) =>
-	import(path, { with: { type: "json" } }).then((m) => m.default).catch(() => null) as Promise<Awaited<R>>;
+	import(path, { with: { type: "json" } })
+		.then((m) => m.default)
+		.catch(() => null) as Promise<Awaited<R | null>>;
 
 export default {
 	en: await j("./en.json"),
